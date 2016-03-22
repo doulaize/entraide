1c76197373837d4c7519bb21656a7a5147fcda46
Rafael Deleuze <rafael_deleuze@hotmail.com>
2016-03-22 22:50:30 +0300

    plutot que de dire qu'un utilisateur n'a pas le droit, tout simplement on ne le laisse pas cliquer sur le bouton

--- index.php
+++ index.php
@@ -29,7 +29,8 @@
                                     document.getElementById('params').style.visibility='hidden';
                                     document.getElementById('params').style.display='none';"
                                     document.getElementById('backup').style.visibility='hidden';
-                                    document.getElementById('backup').style.display='none';>Donn&eacute;es personnelles</a>&nbsp;&nbsp;
+                                    document.getElementById('backup').style.display='none';
+                                    document.getElementById('alert').innerHTML = '';>Donn&eacute;es personnelles</a>&nbsp;&nbsp;
                                     <a href="#" onclick="writediv('');
                                     document.getElementById('sous_menu_data_perso').style.visibility='hidden';
                                     document.getElementById('sous_menu_data_perso').style.display='none';
@@ -42,7 +43,9 @@
                                     document.getElementById('params').style.visibility='hidden';
                                     document.getElementById('params').style.display='none';"
                                     document.getElementById('backup').style.visibility='hidden';
-                                    document.getElementById('backup').style.display='none';>Section Domiciliations</a>&nbsp;&nbsp;
+                                    document.getElementById('backup').style.display='none';
+                                    document.getElementById('alert').innerHTML = '';>Section Domiciliations</a>&nbsp;&nbsp;
+                                    <?if($_SESSION['droits'] != "I"){?>
                                     <a href="#" onclick="writediv('');
                                     document.getElementById('users').style.visibility='visible';
                                     document.getElementById('users').style.display='block';
@@ -55,7 +58,9 @@
                                     document.getElementById('params').style.visibility='hidden';
                                     document.getElementById('params').style.display='none';"
                                     document.getElementById('backup').style.visibility='hidden';
-                                    document.getElementById('backup').style.display='none';>Utilisateurs</a>&nbsp;&nbsp;
+                                    document.getElementById('backup').style.display='none';
+                                    document.getElementById('alert').innerHTML = '';>Utilisateurs</a>&nbsp;&nbsp;
+                                    <?}?>
                                     <?if($_SESSION['droits'] == "T"){?>
                                     <a href="#" onclick="writediv('');
                                     document.getElementById('stats').style.visibility='visible';
@@ -69,7 +74,8 @@
                                     document.getElementById('params').style.visibility='hidden';
                                     document.getElementById('params').style.display='none';"
                                     document.getElementById('backup').style.visibility='hidden';
-                                    document.getElementById('backup').style.display='none';>Statistiques</a>&nbsp;&nbsp;
+                                    document.getElementById('backup').style.display='none';
+                                    document.getElementById('alert').innerHTML = '';>Statistiques</a>&nbsp;&nbsp;
                                     <a href="#" onclick="writediv('');
                                     document.getElementById('params').style.visibility='visible';
                                     document.getElementById('params').style.display='block';
@@ -82,7 +88,8 @@
                                     document.getElementById('stats').style.visibility='hidden';
                                     document.getElementById('stats').style.display='none';"
                                     document.getElementById('backup').style.visibility='hidden';
-                                    document.getElementById('backup').style.display='none';>Param&egrave;tres</a>&nbsp;&nbsp;
+                                    document.getElementById('backup').style.display='none';
+                                    document.getElementById('alert').innerHTML = '';>Param&egrave;tres</a>&nbsp;&nbsp;
                                     <a href="#" onclick="writediv('');
                                     document.getElementById('backup').style.visibility='visible';
                                     document.getElementById('backup').style.display='block';
@@ -95,7 +102,8 @@
                                     document.getElementById('stats').style.visibility='hidden';
                                     document.getElementById('stats').style.display='none';"
                                     document.getElementById('params').style.visibility='hidden';
-                                    document.getElementById('params').style.display='none';>Sauvegarde</a>&nbsp;&nbsp;
+                                    document.getElementById('params').style.display='none';
+                                    document.getElementById('alert').innerHTML = '';>Sauvegarde</a>&nbsp;&nbsp;
                                     <?}?>
                                     <a href="./disconnect.php">D&eacute;connexion</a>
                                 </div>
@@ -121,11 +129,11 @@
                                             <td>
                                                 <a href="#" onclick="document.getElementById('alert').innerHTML = '';writediv(file('./contenu/search_dom.php?droits=<?echo $_SESSION['droits']?>&id_user=<?echo $_SESSION['id'];?>'));">Rechercher</a>
                                             </td>
-                                            <td>||</td>
                                             <td>
-                                                <?if($_SESSION['droits'] == "I"){?>
-                                                <a href="#" onclick="document.getElementById('alert').innerHTML = '';writediv('Vous n\'avez pas les droits');">Cr&eacute;er</a>
-                                                <?}else{?>
+                                                <?if($_SESSION['droits'] != "I"){?>
+                                                ||
+                                            </td>
+                                            <td>
                                                 <a href="#" onclick="document.getElementById('alert').innerHTML = '';writediv(file('./contenu/create_dom.php?droits=<?echo $_SESSION['droits']?>'));">Cr&eacute;er</a>
                                                 <?}?>
                                             </td>
@@ -135,28 +143,23 @@
                             </td>
                             <td>
                                 <div id="users" style="visibility:hidden;display:none;">
-                                    <?if($_SESSION['droits'] != "I"){?>
                                     <table>
                                         <tr>
                                             <td>
-                                                <a href="#" onclick="document.getElementById('alert').innerHTML = '';writediv(file('./contenu/create_user.php?droits=<?echo $_SESSION['droits']?>'));">Cr&eacute;ation</a>		
-                                            </td>
-                                            <?if($_SESSION['droits'] != "C"){?>
-                                            <td>||</td>
-                                            <td>
-                                                <a href="#" onclick="document.getElementById('alert').innerHTML = '';writediv(file('./contenu/modif_user.php?modif_droit=vrai&droits=<?echo $_SESSION['droits']?>'));">Modification</a>
+                                                <a href="#" onclick="document.getElementById('alert').innerHTML = '';writediv(file('./contenu/create_user.php?droits=<?echo $_SESSION['droits']?>'));">Cr&eacute;ation</a>
                                             </td>
                                             <?if($_SESSION['droits'] == "M" || $_SESSION['droits'] == "T"){?>
                                             <td>||</td>
                                             <td>
+                                                <a href="#" onclick="document.getElementById('alert').innerHTML = '';writediv(file('./contenu/modif_user.php?modif_droit=vrai&droits=<?echo $_SESSION['droits']?>'));">Modification</a>
+                                            </td>
+                                            <td>||</td>
+                                            <td>
                                                 <a href="#" onclick="document.getElementById('alert').innerHTML = '';writediv(file('./contenu/del_user.php?droits=<?echo $_SESSION['droits']?>'));">Suppression</a>
                                             </td>
-                                            <?}}?>
+                                            <?}?>
                                         </tr>
                                     </table>
-                                    <?}else{?>
-                                    <script>writediv('vous n\'avez pas les droits');</script>
-                                    <?}?>
                                 </div>
                             </td>
                             <td>
@@ -187,7 +190,7 @@
                                             </td>
                                             <td>||</td>
                                             <td>
-                                                <a href="#" onclick="document.getElementById('alert').innerHTML = '';writediv(file('./contenu/gestion_statut.php'));">gestion des statuts</a>
+                                                <a href="#" onclick="document.getElementById('alert').innerHTML = '';writediv(file('./contenu/gestion_statut.php'));">gestion des statut</a>
                                             </td>
                                             <td>||</td>
                                             <td>
@@ -196,6 +199,25 @@
                                             <td>||</td>
                                             <td>
                                                 <a href="#" onclick="document.getElementById('alert').innerHTML = '';writediv(file('./contenu/gestion_signatures.php'));">gestion des droits de signature</a>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </div>
+                            </td>
+                            <td>
+                                <div id="backup" style="visibility:hidden;display:none;">
+                                    <table>
+                                        <tr>
+                                            <td>
+                                                <a href="#" onclick="document.getElementById('alert').innerHTML = '';writediv(file('./contenu/sauvegarde_bdd.php'));">Sauvegarde BDD sur Disque</a>
+                                            </td>
+                                            <td>||</td>
+                                            <td>
+                                                <a href="#" onclick="document.getElementById('alert').innerHTML = '';writediv(file('./contenu/recuperation_bdd.php'));">R&eacute;cup&eacute;ration BDD depuis Disque</a>
+                                            </td>
+                                            <td>||</td>
+                                            <td>
+                                                <a href="#" onclick="document.getElementById('alert').innerHTML = '';writediv(file('./contenu/sauvegarde_soft.php'));">Sauvegarde Logiciel USB sur Disque</a>
                                             </td>
                                         </tr>
                                     </table>
@@ -211,93 +233,32 @@
                     <tr>
                         <td>
                             <div id="sous_menu_data_perso" style="visibility:hidden;display:none;">
-                                <table>
-                                    <tr>
-                                        <td>
-                                            <a href="#" onclick="document.getElementById('alert').innerHTML = '';
-                                                writediv(file('./contenu/modif_data.php?id=<?echo $_SESSION['id']?>'));">Modification donn&eacute;es</a>
-                                        </td>
-                                    </tr>
-                                </table>
+
                             </div>
                         </td>
                         <td>
                             <div id="section_dom" style="visibility:hidden;display:none;">
-                                <table>
-                                    <tr>
-                                        <td>
-                                            <a href="#" onclick="document.getElementById('alert').innerHTML = '';writediv(file('./contenu/search_dom.php?droits=<?echo $_SESSION['droits'];?>&id_user=<?echo $_SESSION['id'];?>'));">Rechercher</a>
-                                        </td>
-                                        <td>||</td>
-                                        <td>
-                                            <a href="#" onclick="document.getElementById('alert').innerHTML = '';writediv(file('./contenu/create_dom.php?droits=<?echo $_SESSION['droits']?>'));">Cr&eacute;er</a>
-                                        </td>
-                                    </tr>
-                                </table>
+
                             </div>
                         </td>
                         <td>
                             <div id="users" style="visibility:hidden;display:none;">
-                                <?if($_SESSION['droits'] != "I"){?>
-                                <table>
-                                    <tr>
-                                        <td>
-                                            <a href="#" onclick="document.getElementById('alert').innerHTML = '';writediv(file('./contenu/create_user.php?droits=<?echo $_SESSION['droits']?>'));">Cr&eacute;ation</a>		
-                                        </td>
-                                        <?if($_SESSION['droits'] != "C"){?>
-                                        <td>||</td>
-                                        <td>
-                                            <a href="#" onclick="document.getElementById('alert').innerHTML = '';writediv(file('./contenu/modif_user.php?droits=<?echo $_SESSION['droits']?>'));">Modification</a>
-                                        </td>
-                                        <?if($_SESSION['droits'] == "M" || $_SESSION['droits'] == "T"){?>
-                                        <td>||</td>
-                                        <td>
-                                            <a href="#" onclick="document.getElementById('alert').innerHTML = '';writediv(file('./contenu/del_user.php?droits=<?echo $_SESSION['droits']?>'));">Suppression</a>
-                                        </td>
-                                        <?}}?>
-                                    </tr>
-                                </table>
-                                <?}else{?>
-                                <script>writediv('vous n\'avez pas les droits');</script>
-                                <?}?>
+ 
                             </div>
                         </td>
                         <td>
                             <div id="stats" style="visibility:hidden;display:none;">
-                                <table>
-                                    <tr>
-                                        <td>
-                                            <a href="#" onclick="document.getElementById('alert').innerHTML = '';writediv(file('./contenu/stats_user.php'));">Utilisateurs</a>
-                                        </td>
-                                        <td>||</td>
-                                        <td>
-                                            <a href="#" onclick="document.getElementById('alert').innerHTML = '';writediv(file('./contenu/stats_dom.php'));">Domicili&eacute;s</a>
-                                        </td>
-                                    </tr>
-                                </table>
+                            
                             </div>
                         </td>
                         <td>
                             <div id="params" style="visibility:hidden;display:none;">
-                                <table>
-                                    <tr>
-                                        <td>
-                                            <a href="#" onclick="document.getElementById('alert').innerHTML = '';writediv(file('./contenu/gestion_pays.php'));">gestion des pays</a>
-                                        </td>
-                                        <td>||</td>
-                                        <td>
-                                            <a href="#" onclick="document.getElementById('alert').innerHTML = '';writediv(file('./contenu/gestion_nationalite.php'));">gestion des nationalit&eacute;s</a>
-                                        </td>
-                                        <td>||</td>
-                                        <td>
-                                            <a href="#" onclick="document.getElementById('alert').innerHTML = '';writediv(file('./contenu/gestion_statut.php'));">gestion des statuts</a>
-                                        </td>
-                                        <td>||</td>
-                                        <td>
-                                            <a href="#" onclick="document.getElementById('alert').innerHTML = '';window.open('./contenu/gestion_sorties.php');">gestion des sorties</a>
-                                        </td>
-                                    </tr>
-                                </table>
+
+                            </div>
+                        </td>
+                        <td>
+                            <div id="backup" style="visibility:hidden;display:none;">
+
                             </div>
                         </td>
                     </tr>
