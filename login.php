ee2390b038164300ae9362edac174f039b9d36af
Rafael Deleuze <rafael_deleuze@hotmail.com>
2016-03-22 17:11:02 +0300

    rajout d'un else dans la suite logique du login. Si le mdp ou si le login sont vides, pas la peine de continuer

--- login.php
+++ login.php
@@ -4,29 +4,32 @@ if(isset($_POST['login']) && isset($_POST['mdp'])){//si on a passe en parametre 
 		echo '<meta http-equiv="refresh" content="0;url=./index.php?error=1">';
 	//$link = @mysql_connect("sql.free.fr", "xavierrocher", "09071984") //connection du user   //preprod
 	//$link = @mysql_connect("localhost", "xavEntraide", "nmSFBd3fk") //connection du user
-	include("./action_bdd/connexion.php");
-	
-	$query = " SELECT * FROM utilisateurs WHERE login='".htmlentities($_POST['login'],ENT_QUOTES)."' and mdp ='".htmlentities($_POST['mdp'],ENT_QUOTES)."'"; //récupération des droits
-	$res = mysql_query($query);
-	$row = mysql_fetch_array($res);
-		
-	session_start();
-	$_SESSION = array();
-	$_SESSION['id'] = $row['id'];
-	$_SESSION['droits'] = $row['droits'];
-	$_SESSION['nom'] = $row['nom'];
-	$_SESSION['prenom'] = $row['prenom'];
-	$_SESSION['link'] = $link;
-	$_SESSION['connected'] = true;
-	
-	if(isset($_SESSION['id']))
-	{
-		header("Location: ./index.php?success");
-	}
 	else
 	{
-		echo '<meta http-equiv="refresh" content="0;url=./index.php?error=2">';
-		header("Location: ./index.php");
+		include("./action_bdd/connexion.php");
+	
+		$query = " SELECT * FROM utilisateurs WHERE login='".htmlentities($_POST['login'],ENT_QUOTES)."' and mdp ='".htmlentities($_POST['mdp'],ENT_QUOTES)."'"; //récupération des droits
+		$res = mysql_query($query);
+		$row = mysql_fetch_array($res);
+		
+		session_start();
+		$_SESSION = array();
+		$_SESSION['id'] = $row['id'];
+		$_SESSION['droits'] = $row['droits'];
+		$_SESSION['nom'] = $row['nom'];
+		$_SESSION['prenom'] = $row['prenom'];
+		$_SESSION['link'] = $link;
+		$_SESSION['connected'] = true;
+	
+		if(isset($_SESSION['id']))
+		{
+			header("Location: ./index.php?success");
+		}
+		else
+		{
+			echo '<meta http-equiv="refresh" content="0;url=./index.php?error=2">';
+			header("Location: ./index.php");
+		}
 	}
 }
 ?>
