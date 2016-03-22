5b021d7c4b70ed49988d474a8ab057a400d926ed
Rafael Deleuze <rafael_deleuze@hotmail.com>
2016-03-22 17:05:12 +0300

    Rajout code supprime de index.php

--- js/default.js
+++ js/default.js
@@ -23,3 +23,25 @@ function test_saisie_modif_data()
 	}
 	return 1;
 }
+
+if (window.attachEvent) window.attachEvent(“onload”, sfHover);
+if (window.attachEvent) window.attachEvent(“onload”, sfHover2);
+
+function writediv(texte)
+{
+	document.getElementById(‘contenu’).innerHTML = texte;
+}
+
+function file(fichier)
+{
+    if(window.XMLHttpRequest) // FIREFOX
+        xhr_object = new XMLHttpRequest(); 
+    else if(window.ActiveXObject) // IE
+        xhr_object = new ActiveXObject(“Microsoft.XMLHTTP”); 
+    else 
+        return(false); 
+    xhr_object.open(“GET”, fichier, false); 
+    xhr_object.send(null); 
+    if(xhr_object.readyState == 4) return(xhr_object.responseText);
+    else return(false);
+}
